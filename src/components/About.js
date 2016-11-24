d69b174f781a3a2f3749bff359d6111e77f8fab2
Onur Cankaya <oecankaya@gmail.com>
2016-11-24 13:41:11 +0100

    Style about page

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,17 @@ import React from 'react';
 
 export default (props) => {
   return (
-    <h2>About</h2>
+    <div>
+      <h2 className="text-center">About</h2>
+      <p>This is a weather application built using React and Open Weather Map API</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a>
+        </li>
+        <li>
+          <a href="http://opeweathermap.org">Open Weather Map</a>
+        </li>
+      </ul>
+    </div>
   );
 };
