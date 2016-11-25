5fa6ee7c24aae5643e6299bb65af6a06a70e7a69
Onur Cankaya <oecankaya@gmail.com>
2016-11-25 14:28:42 +0100

    Add °C unit

--- src/components/WeatherMessage.js
+++ src/components/WeatherMessage.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default ({temp, location}) => {
   return (
     <div>
-      <h4 className="text-center">It is {temp} degrees in {location}</h4>
+      <h4 className="text-center">It is {temp} °C in {location}</h4>
     </div>
   );
 };
